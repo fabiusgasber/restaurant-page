4221a719003da1063d72deeb7e041bbac60668de
Fabius Gasber <fabiusgasber@gmail.com>
2024-09-06 16:11:51 -0400

    feat: implement tab switch logic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import "./styles.css"
 import { loadHome } from "./home"
+import { loadMenu } from "./menu";
+import { loadContact } from "./contact";
 
-function init() {
-    loadHome();
+
+function tabSwitch(){
+    const body = document.querySelector("body");
+    body.addEventListener("click", loadTab);
 }
 
-init();
+function loadTab(e){
+    console.dir(e.target);
+    const currentTab = e.target.id;
+    const contentDiv = document.querySelector("div#content");
+    contentDiv.innerHTML = "";
+    switch(currentTab) {
+        case "menuBtn":
+        case "homeMenu":
+            loadMenu();
+            break;
+        case "contactBtn":
+            loadContact();
+            break;
+        default: 
+            loadHome();
+            break;
+    }
+}
+
+tabSwitch();
+loadHome();
