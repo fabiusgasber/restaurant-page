3e4c5c89f51e36350bd1a5023fd4b1b55b076b3b
Fabius Gasber <fabiusgasber@gmail.com>
2024-09-10 15:22:16 -0400

    feat: add menu button on home page

--- src/home.js
+++ src/home.js
@@ -7,8 +7,12 @@ export function loadHome() {
 
     const subText = document.createElement("p");
     subText.textContent = "Nestled in the heart of wine country, Terra & Vine celebrates the union of earth’s finest ingredients and handcrafted wines. Our menu showcases locally sourced, seasonal produce, paired perfectly with our curated selection of regional and international wines. At Terra & Vine, we invite you to savor elegant flavors, in a warm and inviting atmosphere. Join us for a culinary journey that pays homage to the land and the vine.";
+    
+    const menuButton = document.createElement("button")
+    menuButton.textContent = "Menu";
+    menuButton.id = "homeMenu";
 
-    const content = [heading, subText]
+    const content = [heading, subText, menuButton]
     homeDiv.append(...content);
 
     const contentDiv = document.querySelector("div#content");
