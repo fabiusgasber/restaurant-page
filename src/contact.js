0c845ac9097da9c36ccca10970f06490e60f8959
Fabius Gasber <fabiusgasber@gmail.com>
2024-09-11 10:58:20 -0400

    feat: create contact form dynamically

--- src/contact.js
+++ src/contact.js
@@ -2,10 +2,28 @@ export function loadContact() {
     const contactDiv = document.createElement("div");
     contactDiv.classList.add("contact");
     
-    const contactDescription = document.createElement("p");
-    contactDescription.textContent = "Here is how you can find us";
-    contactDiv.appendChild(contactDescription);
+    const contactForm = document.createElement("form");
+    contactForm.action = "";
+    contactForm.method = "get";
+    contactForm.id = "contact-form"
+    contactForm.classList.add("contact-form");
+    const firstName = getInputElement("text", "firstName", true, "First Name");
+    const lastName = getInputElement("text", "lastName", true, "Last Name");
+    const email = getInputElement("email", "email", true, "E-Mail");
+    const tel = getInputElement("tel", "tel", true, "Telephone Number");
+    const textarea = document.createElement("textarea");
+    textarea.name = "textarea";
+    textarea.placeholder = "Please write down your message for us"
+    textarea.required = true;
+    const submitBtn = document.createElement("button");
+    submitBtn.setAttribute("form", "contact-form");
+    submitBtn.name = "submit";
+    submitBtn.value = "Submit";
+    submitBtn.textContent = "Submit";
+    const formChilds = [firstName, lastName, email, tel, textarea];
+    contactForm.append(...formChilds);
     
+    contactDiv.append(contactForm, submitBtn);
     const contentDiv = document.querySelector("div#content");
     
     contentDiv.append(contactDiv);
